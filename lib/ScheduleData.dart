c0a51c99567e91108d8ac7a5eb4041ddff3d99ee
altantheprodigy <altanpoke9@gmail.com>
2024-02-22 11:43:50 +0700

    Feat [AP] Responsive

---
+++ lib/ScheduleData.dart
@@ -0,0 +1,6 @@
+List<Map<String, dynamic>> ScheduleData = [
+  {"nama": "Meditasi", "photo": "meditation.png", "jam":"6AM"},
+  {"nama": "WorkOut", "photo": "workout.png", "jam":"3PM"},
+  {"nama": "Jogging", "photo": "joging.png", "jam":"5PM"},
+
+];
